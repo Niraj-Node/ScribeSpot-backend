8d5966c4f1e9bb6e29483b9259d9921c1938e1a9
Niraj-Node <nirajaghera999@gmail.com>
2024-06-24 03:28:14 +0530

    Initialize: Blog REST API project

---
+++ src/main/java/com/scribespot/app/ScribeSpotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scribespot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribeSpotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScribeSpotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scribespot/app/ScribeSpotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scribespot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribeSpotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
